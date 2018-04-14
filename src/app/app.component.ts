4df7601e461e64203bf32b2ecdbb08deeab5e85b
Bram Regelbrugge <bramregelbrugge@gmail.com>
2018-04-14 14:36:07 +0200

    committ all the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div class="center-text">
+  <div
+  class="name"
+  [class.grow]="isPageLoaded"
+  >Bram Regelbrugge</div>
+  </div>
+  `,
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  isPageLoaded = false;
+
+  ngOnInit() {
+    this.isPageLoaded = true;
+    console.log('%c Hi! what are you doing here? ', 'background: #222; color: #bada55');
+  }
 }
